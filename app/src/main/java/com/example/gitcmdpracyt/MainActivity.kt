eedaec9198e95ae94745932dade8024119b92bdd
vinz2112 <hippiefishmalta@gmail.com>
2024-09-16 22:51:19 +0200

    This is our third commit

--- app/src/main/java/com/example/gitcmdpracyt/MainActivity.kt
+++ app/src/main/java/com/example/gitcmdpracyt/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : ComponentActivity() {
 
             println("THIS IS CONTAINED IN MY SECOND COMMIT")
 
+            today is a good day
+
             GitCmdPracYTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
